35adb8c144729e4219f4e79aa82d08f3772799de
Upasana6 <upasna.verma06@gmail.com>
2021-09-28 20:39:04 +0530

    MVC model applied, constant.dart added

---
+++ lib/View/Notes/notes_upload.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotesUpload extends StatelessWidget {
+  const NotesUpload({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.teal,
+    );
+  }
+}
