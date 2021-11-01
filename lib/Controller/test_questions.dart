42491d5b373747213a4951c5546576536a860520
Upasana6 <upasna.verma06@gmail.com>
2021-11-01 22:33:20 +0530

    per question adding page added

---
+++ lib/Controller/test_questions.dart
@@ -0,0 +1,89 @@
+class Question {
+  String? _question;
+  List<String> _options = [];
+  int? _answer;
+  int? _marks;
+  int? _incorrectmarksDeduct;
+  int? _time; // in minute
+
+  Question(String ques, String opt1, String opt2, String opt3, String opt4,
+      int ans, int marks, int marksDeduct, int time) {
+    this._question = ques;
+    this._options = [opt1, opt2, opt3, opt4];
+    this._answer = ans;
+    this._marks = marks;
+    this._time = time;
+    this._incorrectmarksDeduct = marksDeduct;
+  }
+
+  void modifyQuestion(String ques, String opt1, String opt2, String opt3,
+      String opt4, int ans, int marks, int marksDeduct, int time) {
+    this._question = ques;
+    this._options = [opt1, opt2, opt3, opt4];
+    this._answer = ans;
+    this._marks = marks;
+    this._time = time;
+    this._incorrectmarksDeduct = marksDeduct;
+  }
+
+  Map<String, dynamic> getWholeQuestion() {
+    Map<String, dynamic> res = {
+      'Question': this._question,
+      'Options': this._options,
+      'Answer': this._answer,
+      'Marks': this._marks,
+      'Time': this._time
+    };
+    return res;
+  }
+
+  String? get question {
+    return this._question;
+  }
+
+  List<String>? get options {
+    return this._options;
+  }
+
+  int? get answer {
+    return this._answer;
+  }
+
+  int? get marks {
+    return this._marks;
+  }
+
+  int? get time {
+    return this._time;
+  }
+
+  int? get marksDeduct {
+    return this._incorrectmarksDeduct;
+  }
+}
+
+class QuestionList {
+  List<Question> test = [];
+
+  void addNewQuestion(String ques, String opt1, String opt2, String opt3,
+      String opt4, int ans, int marks, int marksDeduct, int time) {
+    Question question =
+        Question(ques, opt1, opt2, opt3, opt4, ans, marks, marksDeduct, time);
+    this.test.add(question);
+  }
+
+  void modifyQuestion(
+      String ques,
+      String opt1,
+      String opt2,
+      String opt3,
+      String opt4,
+      int ans,
+      int marks,
+      int marksDeduct,
+      int time,
+      int questionNumber) {
+    this.test[questionNumber - 1].modifyQuestion(
+        ques, opt1, opt2, opt3, opt4, ans, marks, marksDeduct, time);
+  }
+}
--- lib/View/Quiz/add_questions.dart
+++ lib/View/Quiz/add_questions.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:learning_management/Controller/test_questions.dart';
 import 'package:learning_management/View/Widgets/appbar_with_back_and_menu.dart';
+import 'package:learning_management/View/Widgets/custom_text_field.dart';
+import 'package:learning_management/View/Widgets/extended_appbar.dart';
+import 'package:learning_management/View/Widgets/rounded_rectangle_button.dart';
+import 'package:learning_management/View/Widgets/text.dart';
 import 'package:learning_management/constants.dart';
 
-class AddQuestions extends StatelessWidget {
-  const AddQuestions({Key? key}) : super(key: key);
+class AddQuestions extends StatefulWidget {
+  @override
+  _AddQuestionsState createState() => _AddQuestionsState();
+}
+
+class _AddQuestionsState extends State<AddQuestions> {
+  QuestionList questions = QuestionList();
+
+  TextEditingController questionController = TextEditingController();
+  TextEditingController option1Controller = TextEditingController();
+  TextEditingController option2Controller = TextEditingController();
+  TextEditingController option3Controller = TextEditingController();
+  TextEditingController option4Controller = TextEditingController();
+  TextEditingController answerController = TextEditingController();
+  TextEditingController marksController = TextEditingController();
+  TextEditingController incorrectMarksController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +36,118 @@ class AddQuestions extends StatelessWidget {
               size: size,
               buttonColor: AppColors.white,
               backgroundColor: AppColors.midnightBlue,
-            )
+            ),
+            ExtendedAppbar(
+              size: size,
+              title:
+                  'Question Number ${this.questions.test.isEmpty ? 1 : this.questions.test.length + 1}',
+            ),
+            Flexible(
+              flex: 1,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        textEditingController: this.timeController,
+                        labelText: 'Time (in min)',
+                        keyboardType: TextInputType.number,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: size.width / 2.6,
+                            child: CustomTextField(
+                              textEditingController: this.marksController,
+                              labelText: 'Marks Offered',
+                              keyboardType: TextInputType.number,
+                            ),
+                          ),
+                          Container(
+                            width: size.width / 2.6,
+                            child: CustomTextField(
+                              textEditingController: this.marksController,
+                              labelText: 'Deduct Marks',
+                              keyboardType: TextInputType.number,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      CustomTextField(
+                        textEditingController: this.questionController,
+                        labelText: 'Question',
+                        minLines: 5,
+                      ),
+                      CustomTextField(
+                        textEditingController: this.option1Controller,
+                        labelText: 'Option1',
+                      ),
+                      CustomTextField(
+                        textEditingController: this.option2Controller,
+                        labelText: 'Option2',
+                      ),
+                      CustomTextField(
+                        textEditingController: this.option3Controller,
+                        labelText: 'Option3',
+                      ),
+                      CustomTextField(
+                        textEditingController: this.option4Controller,
+                        labelText: 'Option4',
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      CustomTextField(
+                        textEditingController: answerController,
+                        labelText: 'Correct Option (in number)',
+                        keyboardType: TextInputType.number,
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: size.width / 2.7,
+                            child: RoundedRectangleButton(
+                              size: size,
+                              fillColor: AppColors.midnightBlue,
+                              borderColor: AppColors.midnightBlue,
+                              child: Center(
+                                child: MediumText(
+                                  text: 'Add Next',
+                                  color: AppColors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: size.width / 2.7,
+                            child: RoundedRectangleButton(
+                              size: size,
+                              fillColor: AppColors.midnightBlue,
+                              borderColor: AppColors.midnightBlue,
+                              child: Center(
+                                child: MediumText(
+                                  text: 'Done',
+                                  color: AppColors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
