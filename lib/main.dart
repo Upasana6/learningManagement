f28cc9886e90a7c2a783bfaadf90328155426912
kalpana2001 <83230158+kalpana2001@users.noreply.github.com>
2021-12-06 00:29:50 +0530

    added splash screen code

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,30 @@ import 'package:learning_management/View/Notes/notes_upload.dart';
 import 'package:learning_management/View/Notes/pdf_viewer.dart';
 import 'package:learning_management/View/class_detail_for_teacher.dart';
 import 'package:learning_management/View/classes.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Learning Management',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  _MyAppState createState() => _MyAppState();
+}
 
-      /// Splash Page will be called here
-      // home: PdfViewer(),
-      home: ClassDetailForTeacher(),
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext comtext) {
+    return SplashScreen(
+      seconds: 5,
+      backgroundColor: Colors.white,
+      image: Image.asset('assets/imp.png'),
+      loaderColor: Colors.black,
+      photoSize: 150.0,
+      navigateAfterSeconds: Classes(),
     );
   }
 }
