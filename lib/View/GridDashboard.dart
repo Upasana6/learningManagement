8442ca990918cdbbdd1b07e047f0f9b6bd64838e
kalpana2001 <83230158+kalpana2001@users.noreply.github.com>
2022-01-22 10:31:44 +0530

    Create GridDashboard.dart
    Added dashboard

---
+++ lib/View/GridDashboard.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => new HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff0f084b),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 110,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "e-School",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "Home",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              color: Color(0xffa29aac),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  alignment: Alignment.topCenter,
+                  icon: Image.asset(
+                    "assets/notification.png",
+                    width: 24,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          GridDashboard()
+        ],
+      ),
+    );
+  }
+}
+
+class GridDashboard extends StatelessWidget {
+  Items item1 = new Items(
+    title: "SPM",
+    subtitle: "1 Assignment",
+    event: "Due",
+  );
+
+  Items item2 = new Items(
+    title: "DSP",
+    subtitle: "No Assignment",
+    event: "--",
+  );
+  Items item3 = new Items(
+    title: "ML",
+    subtitle: "1 Assignment ",
+    event: "Due",
+  );
+  Items item4 = new Items(
+    title: "DSA",
+    subtitle: "1 Quiz",
+    event: "Due",
+  );
+  Items item5 = new Items(
+    title: "CD",
+    subtitle: "Test",
+    event: "Upcoming",
+  );
+  Items item6 = new Items(
+    title: "NEW CLASS",
+    subtitle: "Click to join",
+    event: "+",
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    List<Items> myList = [item1, item2, item3, item4, item5, item6];
+    var color = 0xff303f9f;
+    return Flexible(
+      child: GridView.count(
+          childAspectRatio: 1.0,
+          padding: EdgeInsets.only(left: 16, right: 16),
+          crossAxisCount: 2,
+          crossAxisSpacing: 18,
+          mainAxisSpacing: 18,
+          children: myList.map((data) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Color(color), borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.title,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    data.subtitle,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white38,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.event,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+    );
+  }
+}
+
+class Items {
+  String title;
+  String subtitle;
+  String event;
+
+  Items({
+    required this.title,
+    required this.subtitle,
+    required this.event,
+  });
+}
